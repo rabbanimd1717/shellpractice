3b66c54a570a9b06dc057734ea16b573f23a08fa
rabbani <rabbani@gmail.com>
2025-09-11 14:02:08 +0530

    in downloads tere are directory of shell practice

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,9 +7,9 @@ expected_output=${12}
 
 if [ $given_number -ne 13 ]
 then
-    echo "print ${given_number} is equal to 13"
-else
     echo "print ${given_number} is not equal to 13"
+else
+    echo "print ${given_number} is equal to 13"
 fi
 
 #-eq, -ne, -gt, -lt, -ge, -le
