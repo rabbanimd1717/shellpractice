c99d95ba18a0111a60312cc072eaae651a96ceac
rabbani <rabbani@gmail.com>
2025-09-11 14:50:34 +0530

    in downloads tere are directory of shell practice

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,7 @@
 
 USER_ID=$(id -u) # in shell scripting to run the linux commands in shell scripting syntax=$(Linux_commands) in inside the braces we need to write
 
-if [ USER_ID -eq 0 ]
+if [ $USER_ID -eq 0 ]
 then
     echo "Need to give super user permission to access or installing the packages"
     exit 1 #manually exit if error comes
