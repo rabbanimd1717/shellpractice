287e26ce1d58c63165c562f8a50eb3dcd286c33e
rabbani <rabbani@gmail.com>
2025-09-11 14:48:34 +0530

    in downloads tere are directory of shell practice

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,6 +2,16 @@
 
 #package installation
 
+USER_ID=$(id -u) # in shell scripting to run the linux commands in shell scripting syntax=$(Linux_commands) in inside the braces we need to write
+
+if [ USER_ID -eq 0 ]
+then
+    echo "Need to give super user permission to access or installing the packages"
+    exit 1 #manually exit if error comes
+else
+    echo "This is super user and proceed further"
+fi
+
 dnf install mysql -y
 
 if [ $? -eq 0 ]
@@ -10,3 +20,5 @@ then
 else
     echo "MYSQl is not installed"
 fi
+
+echo "MYSQL installed"
