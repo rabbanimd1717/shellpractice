f788a7d8fe496648c130ff3264470c31095e732a
rabbani <rabbani@gmail.com>
2025-09-11 14:53:16 +0530

    in downloads tere are directory of shell practice

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,7 +7,7 @@ USER_ID=$(id -u) # in shell scripting to run the linux commands in shell scripti
 if [ $USER_ID -eq 0 ]
 then
     echo "Need to give super user permission to access or installing the packages"
-    exit 1 #manually exit if error comes
+    #exit 1 #manually exit if error comes
 else
     echo "This is super user and proceed further"
 fi
@@ -16,9 +16,9 @@ dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
-    echo "MYSQL is installed"
+    echo "MYSQL is installation SUCCEESS"
 else
-    echo "MYSQl is not installed"
+    echo "MYSQl Installation FAILURE"
 fi
 
 echo "MYSQL installed"
