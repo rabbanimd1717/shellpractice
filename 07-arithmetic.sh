45ce1d270d7badc30c586cdd779a51b586943470
rabbani <rabbani@gmail.com>
2025-09-11 13:17:33 +0530

    in downloads tere are directory of shell practice

--- 07-arithmetic.sh
+++ 07-arithmetic.sh
@@ -6,9 +6,10 @@ N01=$1
 N02=$2
 
 echo $@
+echo $#
 echo $0
 echo $1
 echo $2
 
-echo "sum of 2 numbers: ${N01+N02}"
-echo "sub of 2 numbers: ${N01-N02}"
+echo "sum of 2 numbers: $((N01+N02))"
+echo "sub of 2 numbers: $((N01-N02))"
